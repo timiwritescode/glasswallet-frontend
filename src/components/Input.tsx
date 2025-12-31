5ff6af34d23d6eee90c83fefe3128b21d7ac5697
Odebode Oluwatimilehin <odebodezion@gmail.com>
2025-12-31 01:01:02 +0100

    feat(login): add toggle to make password visible

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,5 @@
-import type { LucideIcon } from "lucide-react";
+import { Eye, EyeOff, type LucideIcon } from "lucide-react";
+import { useState } from "react";
 
 interface InputProps {
     label: string;
@@ -18,18 +19,33 @@ export function Input(
         value, 
         onChange}: InputProps
 ) {
+
+    const [isVisible, setIsVisible] = useState(false)
+    const inputType = type === 'password' && isVisible ? 'text' : type;
+
     return (
         <div>
             <label className="block text-sm mb-2 text-gray-700 font-medium">{label}</label>
             <div className="relative">
                 <Icon className="absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400"/>
                 <input 
-                type={type}
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
-                className="w-full pl-12 pr-4 py-4 rounded-2xl border-2 border-gray-200 focus:border-purple-500 focus:outline-none transition-colors"
-                required />
+                    type={inputType}
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={onChange}
+                    className="w-full pl-12 pr-4 py-4 rounded-2xl border-2 border-gray-200 focus:border-purple-500 focus:outline-none transition-colors"
+                    required 
+                />
+
+                {type === 'password' && (
+                    <button
+                        type="button"
+                        onClick={() => setIsVisible(!isVisible)}
+                        className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 hover:text-purple-600 transition-colors"
+                    >
+                        {isVisible ? <EyeOff className="w-5 h-5"/> : <Eye className="w-5 h-5"/>}
+                    </button>
+                )}
             </div>
         </div>
     );
